402f142800dd3e608aca7892d2dfdf670002ab4e
kenei's_lab <k_hacks@localhost.localdomain>
2025-06-17 00:21:40 +0300

    updated static files

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -146,8 +146,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = "/static/"
-#STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
+STATIC_ROOT = BASE_DIR / "static"
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
