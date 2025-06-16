98dd0098a6853bb7d97ca46f4feb6c65309a41d8
kenei's_lab <k_hacks@localhost.localdomain>
2025-06-16 23:29:33 +0300

    updated req and bash

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
