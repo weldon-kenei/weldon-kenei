a9e9f9d7519a997ed26fc5c7c89f641b777b4443
Weldon <weldonkenei30@gmail.com>
2025-06-18 00:06:04 +0300

    added gunicorn in requirements

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
 set -o errexit
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
+
 python manage.py migrate
