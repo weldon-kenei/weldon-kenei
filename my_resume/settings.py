617435cf85e878c483088b261b4aa4543f8b87a3
Weldon <weldonkenei30@gmail.com>
2025-06-20 22:16:57 +0300

    debuging server error

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox
 # SECURITY WARNING: don't run with debug turned on in production!
    
 ALLOWED_HOSTS = ['*']
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
